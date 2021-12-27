a6576dbd41c2fbade5f39672bc7b4588d422544f
Maxi Britez <maxi.britez@redb.ee>
2021-12-27 16:38:52 -0300

    add error tests

--- src/test/kotlin/com/kiwi/seed/adapter/controller/PokemonControllerTest.kt
+++ src/test/kotlin/com/kiwi/seed/adapter/controller/PokemonControllerTest.kt
@@ -1,9 +1,12 @@
 package com.kiwi.seed.adapter.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.kiwi.seed.adapter.rest.exception.NotFoundRestClientException
 import com.kiwi.seed.application.usecase.FindPokemonUseCase
+import com.kiwi.seed.config.ErrorCode
 import com.kiwi.seed.config.TestConfig
 import com.kiwi.seed.domain.Pokemon
+import org.bouncycastle.asn1.cms.CMSAttributes.contentType
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
@@ -15,6 +18,7 @@ import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 @DisplayName("Tests cases for pokemon controller")
@@ -41,20 +45,58 @@ class PokemonControllerTest {
         val mockedResponse = PokemonController.PokemonRest(name = someName, type = someType, emptyList())
 
         Mockito
-            .`when`(findPokemonUseCase!!.execute("someName"))
+            .`when`(findPokemonUseCase!!.execute(someName))
             .thenReturn(mockedPokemon)
 
+        mockMvc!!.get("/pokemon/{name}", someName) {
+            contentType = MediaType.APPLICATION_JSON
+            accept = MediaType.APPLICATION_JSON
+        }.andExpect {
+            status { isOk() }
+            content { contentType(MediaType.APPLICATION_JSON) }
+            content { json(objectMapper!!.writeValueAsString(mockedResponse)) }
+        }
+
+    }
+
+    @Test
+    @DisplayName("when name is invalid will return not found")
+    fun testPokemonNotFound() {
+        val invalidName = "invalidName"
+        Mockito
+            .`when`(findPokemonUseCase!!.execute(invalidName))
+            .thenThrow(NotFoundRestClientException(ErrorCode.POKEMON_NOT_FOUND))
+
         mockMvc!!
-            .get(url) {
+            .get(url, invalidName) {
                 contentType = MediaType.APPLICATION_JSON
                 accept = MediaType.APPLICATION_JSON }
-            .andExpect { MockMvcResultMatchers.status().isOk }
-            .andExpect { MockMvcResultMatchers.content().json(objectMapper!!.writeValueAsString(mockedResponse)) }
+            .andExpect {
+                status { isNotFound() }
+            }
+
+    }
+
+    @Test
+    @DisplayName("when unexpected error occurs an internal error will be retrieved")
+    fun testInternalError() {
+        val invalidName = "invalidName"
+        Mockito
+            .`when`(findPokemonUseCase!!.execute(invalidName))
+            .thenThrow(RuntimeException())
+
+        mockMvc!!
+            .get(url, invalidName) {
+                contentType = MediaType.APPLICATION_JSON
+                accept = MediaType.APPLICATION_JSON }
+            .andExpect {
+                status { isInternalServerError() }
+            }
 
     }
 
     companion object {
-        const val url = "/pokemon"
+        const val url = "/pokemon/{name}"
         private fun <T> any(): T = Mockito.any<T>()
 
     }
